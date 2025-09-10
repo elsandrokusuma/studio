0544f24f2bc9f96630c547f1b75dd2d840d8d876
Deva Kusuma <elsandrokusuma@gmail.com>
2025-09-10 14:29:00 +0000

    coba sudah saya ganti favicon.ico denganyang terbaru dan sudah saya edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         
         {/* Favicon links for different devices with cache-busting */}
-        <link rel="icon" href="/favicon.ico?v=5" sizes="any" />
-        <link rel="apple-touch-icon" href="/favicon.ico?v=5" />
+        <link rel="icon" href="/favicon.ico?v=6" sizes="any" />
+        <link rel="apple-touch-icon" href="/favicon.ico?v=6" />
         <link rel="manifest" href="/site.webmanifest" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
