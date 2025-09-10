563a8c840a32489e38bae7205bc72ea21d7d17ff
Deva Kusuma <elsandrokusuma@gmail.com>
2025-09-10 11:23:11 +0000

    <link rel="icon" href="/favicon.ico?v=2" />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
         <title>Stationery Inventory</title>
         <meta name="description" content="Comprehensive inventory and stock management ERP" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <link rel="icon" href="https://i.imgur.com/vofm5PY.png" type="image/png" />
-        <link rel="shortcut icon" href="https://i.imgur.com/vofm5PY.png" type="image/png" />
-        <link rel="apple-touch-icon" href="https://i.imgur.com/vofm5PY.png" />
+        <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/favicon.ico?v=2" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
